4faef0c9704e39501e447e6ac385d4fe31a70984
TeodoraMaruna <maruna.ra117.2018@uns.ac.rs>
2022-06-15 00:42:20 +0200

    feat: change dependency version

--- agent.application/src/main/java/com/xwsbsep/agent/application/service/UserServiceImpl.java
+++ agent.application/src/main/java/com/xwsbsep/agent/application/service/UserServiceImpl.java
@@ -129,10 +129,10 @@ public class UserServiceImpl implements UserService {
     public boolean checkPasswordCriteria(String password, String username) {
         PasswordValidator validator = new PasswordValidator(Arrays.asList(
                 new LengthRule(8, 100),
-                new UppercaseCharacterRule(1),
-                new LowercaseCharacterRule(1),
-                new DigitCharacterRule(1),
-                new SpecialCharacterRule(1),
+//                new UppercaseCharacterRule(1),
+//                new LowercaseCharacterRule(1),
+//                new DigitCharacterRule(1),
+//                new SpecialCharacterRule(1),
                 new WhitespaceRule()));
 
         RuleResult result = validator.validate(new PasswordData(password));
