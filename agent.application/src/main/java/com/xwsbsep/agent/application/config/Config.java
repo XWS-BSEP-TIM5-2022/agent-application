f022b7ce789315e7c0e3b3eefd7586c0625cedd9
Kristina Stojic <stojic.kris@gmail.com>
2022-06-15 00:28:33 +0200

    feat: added https

--- agent.application/src/main/java/com/xwsbsep/agent/application/config/Config.java
+++ agent.application/src/main/java/com/xwsbsep/agent/application/config/Config.java
@@ -14,7 +14,7 @@ public class Config implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:" + env.getProperty("frontend.port"))
+        registry.addMapping("/**").allowedOrigins("https://localhost:" + env.getProperty("frontend.port"))
                 .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE");
     }
 }
