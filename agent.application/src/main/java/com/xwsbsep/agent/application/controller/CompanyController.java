1434f640f60c2d3db839c903952bb9e8b26e032e
sanjadrinic <sanja.drinic@uns.ac.rs>
2022-06-15 12:23:21 +0200

    fix: added authorities in controller

--- agent.application/src/main/java/com/xwsbsep/agent/application/controller/CompanyController.java
+++ agent.application/src/main/java/com/xwsbsep/agent/application/controller/CompanyController.java
@@ -14,6 +14,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,7 +37,7 @@ public class CompanyController {
 
 
     @RequestMapping(method = RequestMethod.POST, value = "/request_registration")
-    //@PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasAuthority('saveRegistrationRequest')")
     public ResponseEntity<?> saveRegistrationRequest(@Valid @RequestBody CompanyRegistrationRequestDTO dto) {
         boolean saved = this.companyRegistrationRequestService.saveRegistrationRequest(dto);
         if (saved){
@@ -48,7 +49,7 @@ public class CompanyController {
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/approve_request/{requestId}")
-    //@PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAuthority('approveRequest')")
     public ResponseEntity<?> approveRequest(@PathVariable Long requestId) {
         boolean approved = this.companyRegistrationRequestService.approveRequest(requestId);
         if (approved){
@@ -60,7 +61,7 @@ public class CompanyController {
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/reject_request/{requestId}")
-    //@PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAuthority('rejectRequest')")
     public ResponseEntity<?> rejectRequest(@PathVariable Long requestId) {
         boolean rejected = this.companyRegistrationRequestService.rejectRequest(requestId);
         if (rejected){
@@ -72,7 +73,7 @@ public class CompanyController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/{companyId}/job_offer")
-    //@PreAuthorize("hasRole('COMPANY_OWNER')")
+    @PreAuthorize("hasAuthority('saveJobOffer')")
     public ResponseEntity<?> saveJobOffer(@Valid @RequestBody JobOfferDTO dto, @PathVariable Long companyId) {
         dto.setCompanyId(companyId);
         boolean saved = this.jobOfferService.saveJobOffer(dto);
@@ -85,28 +86,28 @@ public class CompanyController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/job_offer")
-    //@PreAuthorize("hasRole('COMPANY_OWNER') or hasRole('ADMIN') or hasRole('USER')")
+    @PreAuthorize("hasAuthority('getAllJobOffers')")
     public ResponseEntity<?> getAllJobOffers() {
         log.info("Getting all jobs offers success!");
         return new ResponseEntity(this.jobOfferService.getAll(), HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
-    //@PreAuthorize("hasRole('COMPANY_OWNER') or hasRole('ADMIN') or hasRole('USER')")  // TODO: CHECK ?
+    @PreAuthorize("hasAuthority('getCompanyById')")
     public ResponseEntity<?> getCompanyById(@PathVariable Long id) {
         log.info("Getting company with id: " + id + " success!");
         return new ResponseEntity(this.companyService.findCompanyById(id), HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/requests")
-    //@PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAuthority('getAllRequests')")
     public ResponseEntity<?> getAllRequests() {
         log.info("Getting all requests success!");
         return new ResponseEntity(this.companyRegistrationRequestService.findAll(), HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    //@PreAuthorize("hasRole('COMPANY_OWNER') or hasRole('ADMIN') or hasRole('USER')") å
+    @PreAuthorize("hasAuthority('getAllCompanies')")
     public ResponseEntity<?> getAll() {
         log.info("Getting all companies success!");
         return new ResponseEntity(this.companyService.findAll(), HttpStatus.OK);
@@ -114,7 +115,7 @@ public class CompanyController {
 
 
     @RequestMapping(method = RequestMethod.PUT, value = "/updateCompanyInfo")
-    //@PreAuthorize("hasRole('COMPANY_OWNER')")
+    @PreAuthorize("hasAuthority('updateCompanyInfo')")
     public ResponseEntity<?> updateCompanyInfo(@RequestBody CompanyDTO dto) {
         boolean updated = this.companyService.updateCompanyInfo(dto);
         if (updated){
